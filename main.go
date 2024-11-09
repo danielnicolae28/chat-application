ecc94f95c41f0f7ec2b3ca8fb44ee8b1b58c828d
danielnicolae28 <danielnicolae286@gmail.com>
2024-11-09 23:52:08 +0200

    creating server

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("Hello, Chat app")
+	http.HandleFunc("/", hello)
+	http.ListenAndServe(":8080", nil)
+}
+
+func hello(w http.ResponseWriter, req *http.Request) {
+
+	fmt.Fprintf(w, "hello\n")
 }
